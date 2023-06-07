52a2cca17480f795f7b7b7cb9767ffd66321ec25
vaibhav291 <vaibhavtonge29@gmail.com>
2023-06-08 00:23:28 +0530

    added second enum example

--- C_Sharp_Basics/Program.cs
+++ C_Sharp_Basics/Program.cs
@@ -3,8 +3,8 @@ using C_Sharp_Basics;
 
 public class Program
 {
-    public static void Main(string[] args)
+    /*public static void Main(string[] args)
     {
         EnumExample._enum();
-    }
+    }*/
 }
---
+++ C_Sharp_Basics/Threads_.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basics
+{
+    public class Example
+    {
+        public void Thrd1()
+        {
+            for (char i = 'a'; i < 'g'; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100);
+            }
+        }
+    }
+    public class Threads_
+    {
+        
+        public static void Main(string[] args)
+        {
+            Example example = new Example();
+            Thread x = new Thread(new ThreadStart(example.Thrd1));
+            Thread y = new Thread(new ThreadStart(example.Thrd1));
+
+            x.Start();  
+            y.Start();
+        }
+    }
+}
