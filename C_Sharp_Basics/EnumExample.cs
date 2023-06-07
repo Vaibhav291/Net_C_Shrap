d549f78441d1f6f2945dc10c7e7ef234f392dbc5
vaibhav291 <vaibhavtonge29@gmail.com>
2023-06-08 00:07:19 +0530

    Added C#

---
+++ C_Sharp_Basics/EnumExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basics
+{
+    public class EnumExample
+    {
+        public enum Season { WINTER, SPRING,SUMMER, FALL}
+
+        public static void _enum()
+        {
+            int x = (int)Season.WINTER;
+            int y = (int)Season.SUMMER;
+
+            Console.WriteLine("WINTER = {0}", x);
+            Console.WriteLine("SUMMER = {0}", y);
+        }
+    }
+}
---
+++ C_Sharp_Basics/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using C_Sharp_Basics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        EnumExample._enum();
+    }
+}
